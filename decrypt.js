86776aa54122fd89f7ecc51460ab28b218366caf
Andrey Ambartsumov <an.ambartsumov@gmail.com>
2024-04-08 18:03:22 -0400

    moved answer comparison to server side, modified UI, name -> email

--- decrypt.js
+++ decrypt.js
@@ -12,33 +12,49 @@ app.set("view engine", "ejs");
 app.engine("ejs", ejsMate);
 
 app.get("/", (req, res) => {
-  res.render("main.ejs");
+  res.render("main.ejs", { ceasarAns: "msg", rsaAns: 123 });
 });
 
 app.post("/", async (req, res) => {
   cipher = req.body.cipherName;
   studentName = req.body.studentName;
-  let transporter = nodemailer.createTransport({
-    host: process.env.HOST,
-    port: 587,
-    secure: false, // true for 465, false for other ports
-    auth: {
-      user: "apikey", // ethereal user
-      pass: process.env.SEND_EMAIL_PASS, // ethereal password
-    },
-  });
+  cipherAns = req.body.answer;
+  result = false;
+  if (cipher == "rsa") {
+    if (cipherAns == 123) {
+      result = true;
+      sendEmail(studentName, cipher);
+      console.log("email sent");
+    }
+  } else if (cipher == "ceasar") {
+    if (cipherAns == "msg") {
+      result = true;
+      sendEmail(studentName, cipher);
+      console.log("email sent");
+    }
+  }
 
-  const msg = {
-    from: process.env.FROM_EMAIL, // sender address
-    to: process.env.TO_EMAIL, // list of receivers
-    subject: "Response", // Subject line
-    text: `${studentName} got the ${cipher} question right`, // plain text body
+  sendEmail = async function (studentName, cipher) {
+    let transporter = nodemailer.createTransport({
+      host: process.env.HOST,
+      port: 587,
+      secure: false, // true for 465, false for other ports
+      auth: {
+        user: "apikey", // ethereal user
+        pass: process.env.SEND_EMAIL_PASS, // ethereal password
+      },
+    });
+
+    const msg = {
+      from: process.env.FROM_EMAIL, // sender address
+      to: process.env.TO_EMAIL, // list of receivers
+      subject: "Response", // Subject line
+      text: `${studentName} got the ${cipher} question right`, // plain text body
+    };
+    // send mail with defined transport object
+    const info = await transporter.sendMail(msg);
   };
-  // send mail with defined transport object
-  const info = await transporter.sendMail(msg);
-
-  res.send({ status: true });
-  console.log("Message sent!");
+  res.send({ result });
 });
 
 app.listen(3000, () => console.log("listening on port 3000"));
